6e033535f18d1fefb4c4a7ce514f2620a49a972b
Kishan Silawat <kishan.silawat@skyflow.com>
2022-11-11 17:49:07 +0530

    Added polling function

---
+++ lambda_handler.py
@@ -0,0 +1,69 @@
+import json
+import os
+from time import sleep
+
+import boto3
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+
+def lambda_handler(event, context):
+    connection = psycopg2.connect(
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT"),
+        database=os.environ.get("DB_NAME"),
+    )
+
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+
+    client = boto3.client("datasync", region_name=event["region_name"])
+
+    status = client.describe_task_execution(
+        TaskExecutionArn=event["task_execution_arn"],
+    ).get("Status")
+
+    data_transfer_state_update_query = "UPDATE emr_job_details SET " \
+                                       "data_transfer_state=%s WHERE id=%s"
+
+    cursor.execute(
+        data_transfer_state_update_query,
+        (status, event["id"])
+    )
+
+    print(
+        f"""{event["id"]}-> Polling jobstatus: {status}"""
+    )
+    if status in ("SUCCESS", "ERROR"):
+        status = status if status != "ERROR" else "FAILED"
+
+        print(
+            f"""{event["id"]}-> Updating jobstatus: {status}"""
+        )
+        update_query = "UPDATE emr_job_details SET jobstatus=%s WHERE id=%s"
+        cursor.execute(
+            update_query,
+            (status, event["id"])
+        )
+        connection.commit()
+        cursor.close()
+        return
+
+    sleep_interval = int(os.environ.get("POLLING_INTERVAL"))
+
+    print(f""""{event["id"]}-> Sleeping for {sleep_interval} seconds""")
+    sleep(sleep_interval)
+
+    payload = {
+        "id": event["id"],
+        "region_name": event["region_name"],
+        "task_execution_arn": event["task_execution_arn"],
+    }
+    payload_bytes = json.dumps(payload).encode('utf-8')
+
+    print(f"""{event["id"]}-> Performing recursive invocation""")
+    boto3.client("lambda").invoke_async(
+        FunctionName=os.environ.get("POLLING_FUNCTION_ARN"),
+        InvokeArgs=payload_bytes,
+    )
